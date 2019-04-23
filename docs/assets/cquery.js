bb6ffdce0e91d9c85c61973d22ea85a4842e389d
Virx <ericmveilleux@gmail.com>
2019-04-23 13:50:13 -0400

    Update cquery.js

--- docs/assets/cquery.js
+++ docs/assets/cquery.js
@@ -14,13 +14,39 @@ function $(query) {
 		if (query.length == 2) {
 			class = true;
 			query = query[1];
-			element = document.getElementByClass(query);
+			element = document.getElementsByClassName(query);
 		} else  {
 			tag = true;
+			element = document.getElementsByTagName(query);
 		}
 	}
 	
-	var checked = function() {
-		
+	var checked = function(value) {
+		if (value == undefined) {
+			return element.checked;
+		}
+		element.checked = value;
+	}
+	
+	var click = function(code) {
+		element.addEventListener("click", code);
+	}
+	
+	var val = function() {
+		return element.value;
+	}
+}
+function store(item) {
+	var local = function(item2) {
+		if (item2 == undefined) {
+			return localStorage.getItem(item);
+		}
+		localStorage.setItem(item, item2);
+	}
+	var session = function(item2) {
+		if (item2 == undefined) {
+			return sessionStorage.getItem(item);
+		}
+		sessionStorage.setItem(item, item2);
 	}
 }
