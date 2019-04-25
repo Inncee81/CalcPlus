e72b080651b2337d5133826def651b02f17e8f87
Virx <ericmveilleux@gmail.com>
2019-04-25 16:36:11 -0400

    Update 1.15.js

--- docs/assets/CPquery/1.15.js
+++ docs/assets/CPquery/1.15.js
@@ -27,19 +27,19 @@ function $(query) {
 			}
 
 			var css = {
-				append: function(wndw, item) {
-					var sheet = wndw.document.styleSheets[parseInt(query)];
+				append: function(item) {
+					var sheet = window.document.styleSheets[parseInt(query)];
 					sheet.insertRule(item, sheet.cssRules.length);
 				},
-				replace: function(wndw, item) {
-					var sheet = wndw.document.styleSheets[parseInt(query)];
+				replace: function(item) {
+					var sheet = window.document.styleSheets[parseInt(query)];
 					for (var i=0; i<sheet.cssRules.length; i++) {
 						sheet.deleteRule(i);
 					}
 					sheet.insertRule(item, sheet.cssRules.length);
 				},
-				delete: function(wndw, index) {
-					var sheet = wndw.document.styleSheets[query];
+				delete: function(index) {
+					var sheet = window.document.styleSheets[query];
 					sheet.deleteRule(index);
 				}
 			};
