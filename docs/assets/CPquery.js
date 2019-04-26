53895801f235f750fcec304fc14861a6a8350080
Virx <ericmveilleux@gmail.com>
2019-04-26 16:07:31 -0400

    Update CPquery.js

--- docs/assets/CPquery.js
+++ docs/assets/CPquery.js
@@ -76,7 +76,7 @@ function $(query) {
 			
 			var me = function() {
 				if (local) {
-					console.log(localStorage.getItem(query);
+					console.log(localStorage.getItem(query));
 					return localStorage.getItem(query);
 				} else if (session) {
 					return sessionStorage.getItem(query);
