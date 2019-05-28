c83a9cbef216a471802ce940b24756e35269e831
Virx <ericmveilleux@gmail.com>
2019-05-28 15:57:08 -0400

    Update Options.js

--- docs/assets/Options.js
+++ docs/assets/Options.js
@@ -35,15 +35,15 @@ loadScript("assets/CPquery.js", function(){
             $(wndw).css.replace(0, 'body { color: white; background-color: black; }');
             $(wndw).css.append(0, 'a { color: rgb(0, 0, 255); }');
             $(wndw).css.append(0, 'span.broken { color: red; }');
-            $(wndw).css.append(0, 'span.fix { color: rbg(205, 205, 0); }');
+            $(wndw).css.append(0, 'span.fix { color: rgb(205, 205, 0); }');
             $(wndw).css.append(0, 'span.verify { color: orange; }');
             $(wndw).css.append(0, 'span.working { color: green; }');
             $(wndw).css.append(0, '.removeInput { color: black; background-color: red; border: none; }');
         } else {
             $(wndw).css.replace(0, 'body { color: black; background-color: white; }');
-            $(wndw).css.append(0, 'a { color: rbg(0, 0, 192); }');
+            $(wndw).css.append(0, 'a { color: rgb(0, 0, 192); }');
             $(wndw).css.append(0, 'span.broken { color: red; }');
-            $(wndw).css.append(0, 'span.fix { color: rbg(205, 205, 0); }');
+            $(wndw).css.append(0, 'span.fix { color: rgb(205, 205, 0); }');
             $(wndw).css.append(0, 'span.verify { color: orange; }');
             $(wndw).css.append(0, 'span.working { color: green; }');
             $(wndw).css.append(0, '.removeInput { color: black; background-color: red; border: none; }');
@@ -52,6 +52,7 @@ loadScript("assets/CPquery.js", function(){
     }
 
     offline = function(wndw) {
+        console.log("Ran offline(wndw)");
         isOffline = $("*isOffline");
         alerted = $("@alerted");
         //navigator.serviceWorker.getRegistration().then(function(registration) {
@@ -69,37 +70,11 @@ loadScript("assets/CPquery.js", function(){
                 wndw.alert("Service Workers aren't supported by your browser.\nPlease switch to another browser like Chrome.\nIf you are, then please make sure your browser is updated.");
                 alerted.set("true");
             }
-        } else {
-            console.log("Not re-registering Service Workers as it's already been done.");
         }
     }
 });
 
 function load(wndw) {
-    var c = document.querySelector(".console");
-    console.log = (...args) => args.forEach(m => c.appendChild(document.createTextNode(`\n ${m}`)));
-    console.info = (...args) => args.forEach(e => {
-        const s = document.createElement("span");
-            s.textContent = "\n" + e;
-            s.style.color = "blue";
-            c.appendChild(s);
-    });
-    console.warn = (...args) => args.forEach(e => {
-        const s = document.createElement("span");
-            s.textContent = "\n" + e;
-            s.style.color = "rgb(205, 205, 0)";
-            c.appendChild(s);
-    });
-    console.error = (...args) => args.forEach(e => {
-        const s = document.createElement("span");
-            s.textContent = "\n" + e;
-            s.style.color = "red";
-            c.appendChild(s);
-    });
-
-    wndw.onerror = (e, s, l, c) => {
-        console.error(`${e} at: ${s} : ${l}:${c}`);
-    };
     wndw.onload = function() {
         dark(wndw);
         offline(wndw);
