572015a33bfbb25d5b0c8b0770aa71835a429c34
Virx <ericmveilleux@gmail.com>
2019-09-10 15:39:52 -0400

    Update Options.js

--- assets/Options.js
+++ assets/Options.js
@@ -64,6 +64,35 @@ function loadOptions(){
         }
     });
     if (isConsole.me() == "On") {
-        // place the code for the console here
+        var c = document.querySelector(".console");
+        console.log = (...args) => args.forEach(m => {
+            try {
+                c.appendChild(document.createTextNode(`\n ${m}`));
+          } catch(err) {
+                console.log(err);
+          }
+        });
+        console.warn = (...args) => args.forEach(e => {
+            try {
+                const s = document.createElement("span");
+                    s.textContent = "\n" + e;
+                    s.style.color = "rgb(205, 205, 0)";
+                    c.appendChild(s);
+            } catch(err) {
+                console.warn(err);
+            }
+        });
+        console.error = (...args) => args.forEach(e => {
+            try {
+                const s = document.createElement("span");
+                    s.textContent = "\n" + e;
+                    s.style.color = "red";
+                    c.appendChild(s);
+            } catch(err) {
+                console.error(err);
+            }
+        });
+
+        window.onerror=(e,s,l,c)=>console.error(`${e} at: ${s} : ${l}:${c}`);
     }
 }
