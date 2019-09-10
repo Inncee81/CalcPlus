e2aecd6c55f50be9221bcf86461f3dcadea1f14b
Virx <ericmveilleux@gmail.com>
2019-09-10 07:15:24 -0400

    Update Options.js

--- assets/Options.js
+++ assets/Options.js
@@ -67,36 +67,7 @@ var loadOptions = function(){
         } else console.info("Service Workers already registered.");
     });
     if (isConsole.me() == "On") {
-        var c = document.querySelector(".console");
-        console.log = (...args) => args.forEach(m => {
-            try {
-                c.appendChild(document.createTextNode(`\n ${m}`));
-            } catch(err) {
-                console.log(err);
-            }
-        });
-        console.warn = (...args) => args.forEach(e => {
-            try {
-                const s = document.createElement("span");
-                    s.textContent = "\n" + e;
-                    s.style.color = "rgb(205, 205, 0)";
-                    c.appendChild(s);
-            } catch(err) {
-                console.warn(err);
-            }
-        });
-        console.error = (...args) => args.forEach(e => {
-            try {
-                const s = document.createElement("span");
-                    s.textContent = "\n" + e;
-                    s.style.color = "red";
-                    c.appendChild(s);
-            } catch(err) {
-                console.error(err);
-            }
-        });
-
-        window.onerror=(e,s,l,c)=>console.error(`${e} at: ${s} : ${l}:${c}`);
+        // place the code for the console here
     }
 }
 
