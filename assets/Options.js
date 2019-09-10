671c50cd8e64dd3921bd0ca8374014fb333fe902
Virx <ericmveilleux@gmail.com>
2019-09-10 08:13:23 -0400

    Update Options.js

--- assets/Options.js
+++ assets/Options.js
@@ -1,7 +1,7 @@
-var script = document.createElement("script");
+var script = window.document.createElement("script");
 script.src = (sessionStorage.getItem("index") == "On")?"assets/CPquery.js":"../assets/CPquery.js";
 script.onload = script.onreadystatechange = start;
-document.head.appendChild(script);
+window.document.head.appendChild(script);
 var start = function() { loadOptions(); };
 function loadOptions(){
     var isDark = $("@isDark"), isOffline = $("*isOffline"), alerted = $("*alerted"), isConsole = $("@isConsole"), savei = $("@isSaveI"), isUndefined = setting => { !(setting.me() == "Off" || setting.me() == "On"); };
