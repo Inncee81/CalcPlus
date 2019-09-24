4e7873075602b85f53f4bd38d1a1cb91ff8e5127
Virx <ericmveilleux@gmail.com>
2019-09-24 17:41:12 -0400

    Update Library.js

--- assets/Library.js
+++ assets/Library.js
@@ -87,7 +87,7 @@ function parseNums(num1, num2, mode) {
 		else if (parseInt(num1[i]) > parseInt(num2[i])) skip = true;
 	}
 
-	if (mode == 3) num1 = num1.join(''), num2 = num2.join('');
+	if (mode == 3 || mode == 1) num1 = num1.join(''), num2 = num2.join('');
 	return {
 		num1: {
 			num: num1,
